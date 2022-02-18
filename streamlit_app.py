ed23fc6c596bfe063d3010d7513fe9df6030699a
Robert Uwe Litschel <uwelitschel@gmail.com>
2022-02-18 09:38:35 +0100

    added google sheets data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+from gsheetsdb import connect
+
 import geemap.foliumap as geemap
 from geopy.geocoders import Nominatim 
 #To avoid time out in API
@@ -14,6 +17,46 @@ st.set_page_config(page_title='Immo-FFM-Demo',
                 #menu_items: Get help, Report a Bug, About
 
 st.title("hello 3")
+
+
+# get data from google sheet
+#------------------
+
+# Share the connector across all users connected to the app
+@st.experimental_singleton()
+def get_connector():
+    return connect()
+
+# Time to live: the maximum number of seconds to keep an entry in the cache
+TTL = 24 * 60 * 60
+
+# Using `experimental_memo()` to memoize function executions
+@st.experimental_memo(ttl=TTL)
+def query_to_dataframe(_connector, query: str) -> pd.DataFrame:
+    rows = _connector.execute(query, headers=1)
+    dataframe = pd.DataFrame(list(rows))
+    return dataframe
+
+@st.experimental_memo(ttl=600)
+def get_data(_connector, gsheets_url) -> pd.DataFrame:
+    return query_to_dataframe(_connector, f'SELECT * FROM "{gsheets_url}"')
+
+
+gsheet_connector = get_connector()
+gsheets_url = st.secrets["gsheets"]["public_gsheets_url"]
+data = get_data(gsheet_connector, gsheets_url)
+df = pd.DataFrame(data)
+
+
+# overiew
+#------------------
+st.header("Overview")
+with st.expander("Click to see basic stats!"):
+    st.write(df.describe())
+
+
+
+
 
 #------------------
 # get input
